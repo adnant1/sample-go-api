4c1a37da18befea4cad40657a62c88b62bf41fec
adnant1 <adnantayib8@gmail.com>
2025-09-13 19:06:19 -0400

    Created params, res, error structs for the api

--- api/api.go
+++ api/api.go
@@ -1,1 +1,18 @@
 package api
+
+// Coin Balance params
+type CoinBalanceParams struct {
+	Username string
+}
+
+// Coin Balance response
+type CoinBalanceResponse struct {
+	Code int // Success code, usually 200
+	Balance int64 // Account balance
+}
+
+// Error reponse
+type Error struct {
+	Code int // Error code
+	Message string // Error message
+}
