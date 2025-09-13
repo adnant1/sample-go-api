333a3c43b3b74ac361daaf66a6dbe772048f9ed7
adnant1 <adnantayib8@gmail.com>
2025-09-13 19:38:52 -0400

    feat: Set up GET /account/coins in Handler

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -1,1 +1,22 @@
 package handlers
+
+import (
+	"github.com/adnant1/sample-go-api/internal/middleware"
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+)
+
+// Capital letter H in Handler makes it public
+func Handler(r *chi.Mux) {
+	// Global middleware
+	r.Use(chimiddle.StripSlashes)
+
+	r.Route("/account", func(router chi.Router) {
+
+		// Middleware for /account route
+		router.Use(middleware.Authenticate)
+
+		// GET /account/coins
+		router.Get("/coins", GetCoinBalance)
+	})
+}
