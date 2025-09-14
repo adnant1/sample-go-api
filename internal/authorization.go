5234a06ced355ded91e616b56d2b451cd69bb5d5
adnant1 <adnantayib8@gmail.com>
2025-09-13 20:11:50 -0400

    Implemented Authorization function

--- internal/authorization.go
+++ internal/authorization.go
@@ -1,1 +1,51 @@
 package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/adnant1/sample-go-api/api"
+	"github.com/adnant1/sample-go-api/internal/tools"
+	log "github.com/sirupsen/logrus"
+)
+
+// Global unauthorized error
+var UnAuthorizedError = errors.New("Invalid username or token.")
+
+// Authorization middleware
+func Authorization(next http.Handler) http.Handler {
+	// Return a handler function
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		
+		var username string = r.URL.Query().Get("username")
+		var token = r.Header.Get("Authorization")
+		var err error
+		
+		// Check if username and token are valid
+		if username == "" || token == "" {
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
+		}
+
+		// Query the database for user login details
+		var database *tools.DatabaseInterface
+		database, err = tools.NewDatabase()
+		if err != nil {
+			api.InternalErrorHandler(w)
+			return
+		}
+
+		var loginDetails *tools.loginDetails
+		loginDetails = (*database).GetUserLoginDetails(username)
+
+		if (loginDetails == nil || (token != (*loginDetails).AuthToken)) {
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
+		}
+
+		// Call the next handler if authorized
+		next.ServeHTTP(w, r)
+	})
+}
