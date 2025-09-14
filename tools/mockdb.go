5a36d664fff915de5a4fc776f3d5325ab096c1c6
adnant1 <adnantayib8@gmail.com>
2025-09-13 20:48:53 -0400

    Added mock data

--- tools/mockdb.go
+++ tools/mockdb.go
@@ -1,1 +1,35 @@
 package tools
+
+type mockDB struct {}
+
+// Mock user login details
+var mockLoginDetails = map[string]LoginDetails {
+	"alex": {
+		AuthToken: "123ABC",
+		Username: "alex",
+	},
+	"jason": {
+		AuthToken: "456DEF",
+		Username: "jason",
+	},
+	"giselle": {
+		AuthToken: "789GHI",
+		Username: "giselle",
+	},
+}
+
+// Mock user coin details
+var mockCoinDetails = map[string]CoinDetails {
+	"alex": {
+		Coins: 100,
+		Username: "alex",
+	},
+	"jason": {
+		Coins: 250,
+		Username: "jason",
+	},
+	"giselle": {
+		Coins: 500,
+		Username: "giselle",
+	},
+}
